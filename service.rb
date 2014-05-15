e79b6637e6a9818c7e13073fb2fc00352108644b
Soumya Ray <soumya.ray@gmail.com>
2014-05-15 15:01:14 +0800

    modified YAML loading of database.yml to run it through ERB for heroku

--- service.rb
+++ service.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 require 'sinatra'
-#require './environments.rb'
+require 'erb'
 require_relative 'models/user.rb'
 require 'logger'
 
@@ -16,7 +16,7 @@ log.debug "env: #{env}"
 
 # connecting to the database
 use ActiveRecord::ConnectionAdapters::ConnectionManagement # close connection to the DDBB properly...https://github.com/puma/puma/issues/59
-databases = YAML.load_file("config/database.yml")
+databases = YAML.load(ERB.new(File.read("config/database.yml")).result)
 ActiveRecord::Base.establish_connection(databases[env])
 log.debug "#{databases[env]['database']} database connection established..."
 
