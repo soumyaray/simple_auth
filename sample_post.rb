c6da695ae01754e9944b1a966d93fdf479e5a43d
Soumya Ray <soumya.ray@gmail.com>
2014-05-21 00:55:41 +0800

    Cleaned up and commented sample_post.rb

--- sample_post.rb
+++ sample_post.rb
@@ -2,11 +2,22 @@ require 'net/HTTP'
 require 'json'
 require 'openssl'
 
+# Setup URI for HTTP connection
 uri = URI('https://simple-auth.herokuapp.com/api/v1/users')
-
 http = Net::HTTP.new(uri.host, uri.port)
+
+# Setup for HTTPS connection
 http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
-req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
-req.body = {name: 'Raju', email:'thimmarajus@gmail.com', password:'thisworks', bio:'TA'}.to_json
+# TODO: use VERIFY_PEER verification mode in production environment
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# Setup HTTP request object
+req = Net::HTTP::Post.new(uri,
+  initheader = {'Content-Type' =>'application/json'})
+req.body = {
+  name: 'Raju', email:'thimmarajus@gmail.com',
+  password:'thisworks', bio:'TA'
+}.to_json
+
+# Send request and wait for HTTP response
 res = http.request(req)
